41c43110d41f315310553fa54e61ade302be564c
DeadlyBunny <juajogar@espol.edu.ec>
2016-07-05 19:13:18 -0500

    Threadpool implementado

--- ClienteEspolKeyValueStore.java
+++ ClienteEspolKeyValueStore.java
@@ -8,13 +8,19 @@ public class ClienteEspolKeyValueStore
 		/* args[0] corresponde a la ip del servidor (127.0.0.1 es una referencia a la misma maquina) */
 		/* args[1] es el puerto que escucha el servidor*/
 			Socket sock = new Socket(args[0],Integer.parseInt(args[1]));
+			PrintWriter out=null;
+			out=new PrintWriter(sock.getOutputStream(),true);
 			InputStream in = sock.getInputStream();
 			BufferedReader bin = new
 			BufferedReader(new InputStreamReader(in));
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 			/* read the date from the socket */
 			String line;
-			while ( (line = bin.readLine()) != null)
-			System.out.println(line);
+			while ( (line = stdIn.readLine()) != null){
+					out.println(line);
+					System.out.println(bin.readLine());	
+			}
+
 			/* close the socket connection*/
 			sock.close();
 		}
--- EspolKeyValueStore.java
+++ EspolKeyValueStore.java
@@ -1,25 +1,62 @@
 import java.net.*;
 import java.io.*;
+import java.util.concurrent.*;
+
 public class EspolKeyValueStore
 {
 	public static void main(String[] args) {
 		try {
+		ExecutorService executorService = Executors.newFixedThreadPool(20);
 		/* args[0] corresponde al puerto donde escucha el servidor */
 		ServerSocket sock = new ServerSocket(Integer.parseInt(args[0]));
 		/* now listen for connections */
 			while (true) {
 				Socket client = sock.accept();
-				PrintWriter pout = new
-				PrintWriter(client.getOutputStream(), true);
+				executorService.execute(new tpool(client));
+
+				//PrintWriter pout = new
+				//PrintWriter(client.getOutputStream(), true);
 				/* write the Date to the socket */
-				pout.println(new java.util.Date().toString());
+				//pout.println(new java.util.Date().toString());
 				/* close the socket and resume */
 				/* listening for connections */
-				client.close();
+				//client.close();
 			}
 		}
 		catch (IOException ioe) {
 			System.err.println(ioe);
+		}
+	}
+}
+class tpool implements Runnable{
+
+	private Socket socket;
+
+	public tpool (Socket s){
+		this.socket = s;
+	}
+
+	public void run(){
+		BufferedReader reader = null;
+		PrintWriter writer = null;
+		try{
+			reader = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+			writer = new PrintWriter(socket.getOutputStream(),true);
+			while(true){
+				String line = reader.readLine();
+				if(line.equals("quit")){
+					break;
+				}
+				writer.println("Echo:"+line);
+			}
+		}catch (IOException e){
+			throw new RuntimeException (e);
+		}finally{
+			try{
+				if(reader != null){ reader.close();}
+				if(writer != null){ writer.close();}
+			}catch(IOException e){
+			}
 		}
 	}
 }
