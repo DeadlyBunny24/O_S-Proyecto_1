383835b32f7b794b600be3230e8b667bb0228722
jorgermurillo <jorgermurillo@gmail.com>
2016-07-07 00:53:55 -0500

    Aqui esta la implementacion del servidor con la clase Mapa que contiene el Hashmap y los locks para hacer las operaciones thread-safe.

--- EspolKeyValueStore.java
+++ EspolKeyValueStore.java
@@ -1,9 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package so_project;
+
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class EspolKeyValueStore
 {
+	
+	public static Mapa map = new Mapa();
+	
 	public static void main(String[] args) {
 		try {
 		ExecutorService executorService = Executors.newFixedThreadPool(20);
@@ -44,10 +60,36 @@ class tpool implements Runnable{
 			writer = new PrintWriter(socket.getOutputStream(),true);
 			while(true){
 				String line = reader.readLine();
-				if(line.equals("quit")){
-					break;
+				String[] tokens = line.split(" ");
+				
+			switch(tokens[0]){
+					
+					case "get":
+                                            if(EspolKeyValueStore.map.containsKey(tokens[1])){
+                                                writer.println(EspolKeyValueStore.map.get(tokens[1]));
+                                                break;
+                                            }else{
+                                                writer.println(tokens[1]+"=");
+                                                break;
+                                            }
+                                        case "set":
+                                            EspolKeyValueStore.map.set(tokens[1], tokens[2]);
+                                            writer.println("OK");
+                                            break;
+                                        case "del":
+                                            EspolKeyValueStore.map.del(tokens[1]);
+                                            writer.println("OK");
+                                            break;
+                                        case "list":
+                                            writer.println(EspolKeyValueStore.map.list());
+                                            break;
 				}
-				writer.println("Echo:"+line);
+				
+				
+				/*if(line.equals("quit")){
+					break;
+				}*/
+				//writer.println("Echo:"+line);
 			}
 		}catch (IOException e){
 			throw new RuntimeException (e);
@@ -59,4 +101,86 @@ class tpool implements Runnable{
 			}
 		}
 	}
+
+
+class Mapa {
+    
+    private final HashMap<String,String> map = new HashMap<>();
+    private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    private final Lock r = rwl.readLock();
+    private final Lock w = rwl.writeLock();
+    
+    public String get(String key){
+        String s ="";
+        
+        r.lock();
+            try{
+                s = map.get(key);
+                                
+            }finally{
+                r.unlock();
+            }
+            if(s ==null){
+                return "key=";
+            }else{
+                return  key +"=" + s;
+            }
+        
+    }
+    
+    public Boolean containsKey(String key){
+        
+        return map.containsKey(key);
+                
+    }
+    
+    public void set(String key, String value){
+        
+        w.lock();
+        try{
+            map.put(key, value);
+        }finally{
+            w.unlock();
+        }
+        
+    }
+    
+    
+    public void del(String key){
+        
+        w.lock();
+        try{
+            map.remove(key);
+        }finally{
+            w.unlock();
+        }
+        
+    }
+    
+    public String list(){
+        //O usare un TreeSet
+        Set<String> set = new HashSet<>();
+        
+        r.lock();
+        try{
+           set = map.keySet();
+           
+        }finally{
+            r.unlock();
+        }
+        
+        String[] keys = set.toArray(new String[0]);
+        Arrays.sort(keys);
+        String listaDeKeys="";
+        
+        for(String s:keys){
+            listaDeKeys += s + " ";
+        }
+        
+        return listaDeKeys;
+    }
+}
+
+
+
 }
